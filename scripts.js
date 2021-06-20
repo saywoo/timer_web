fd2031955d796084f1b604f11538d3aad95cb3a4
saywoo <saywoo5121@gmail.com>
2021-06-20 21:04:08 +0900

    2021/06/20 기초 베이스 완성

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
+function time_for_speech(char_cnt) {
+    const char_per_second = 4.5;
+    var sec = parseInt(char_cnt / char_per_second);
+    var min = parseInt(sec / 60);
+    
+    if (min.toString() == "0") {
+        return sec.toString() + "초";
+    }
+    else {
+        var tmp_sec = sec - min * 60;
+        return min.toString() + "분 " + tmp_sec.toString() + "초";
+    }
+}
+
 function char_count() {
-    document.getElementById('char_count_results').innerText = document.getElementById('textbox').value.length;
+    var char_cnt = document.getElementById('textbox').value.length;
+    document.getElementById('time_results').innerText = time_for_speech(char_cnt);
+    document.getElementById('char_count_results').innerText = char_cnt + "자";
 }
 function resize(obj) {
     obj.style.height = '1px';
