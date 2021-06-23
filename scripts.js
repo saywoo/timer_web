6d971d3fdef27384b0b3a8fd6446dfed1915e87e
saywoo <saywoo5121@gmail.com>
2021-06-23 15:05:21 +0900

    2021/06/23 UI 변경

--- scripts.js
+++ scripts.js
@@ -16,6 +16,7 @@ function char_count() {
     var char_cnt = document.getElementById('textbox').value.length;
     document.getElementById('time_results').innerText = time_for_speech(char_cnt);
     document.getElementById('char_count_results').innerText = char_cnt + "자";
+    document.getElementById("real_char_count_results").innerText = document.getElementById('textbox').value.replace(/(\s*)/g, "").length + "자";
 }
 function resize(obj) {
     obj.style.height = '1px';
