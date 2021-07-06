76beb7ef2041eafc3c4e038ab5d4c05ac36d0720
saywoo <saywoo5121@gmail.com>
2021-07-06 22:46:21 +0900

    1

--- scripts.js
+++ scripts.js
@@ -12,9 +12,19 @@ function time_for_speech(char_cnt) {
     }
 }
 
+function byte_cnt(chr) {
+    str_byte_len = 0;
+    str_byte_len = (function(s, b, i, c) {
+        for (b = i = 0; c = s.charCodeAt(i++); b += c>>11?3:c>>7?2:1);
+        return b
+    })(chr);
+    return str_byte_len.toString() + "바이트";
+}
+
 function char_count() {
     var char_cnt = document.getElementById('textbox').value.length;
     document.getElementById('time_results').innerText = time_for_speech(char_cnt);
+    document.getElementById('byte_count_results').innerText = byte_cnt(document.getElementById('textbox').value);
     document.getElementById("char_count_results").innerText = document.getElementById('textbox').value.replace(/(\s*)/g, "").length + "자";
 }
 function resize(obj) {
