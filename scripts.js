c381dd1653b7bc7ade678398a73f74d4df9cc2f8
saywoo <saywoo5121@gmail.com>
2021-07-09 15:43:31 +0900

    1

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+/*
+잡다한 함수들을 저장해놓은 스크립트
+*/
+
+// 분당 몇 글자를 읽는다는 결과를 통해 예상시간을 계산해서 return시키는 함수
 function time_for_speech(res) {
     var char_cnt = document.getElementById('textbox').value.replace(/(\s*)/g, "").length;
     var char_per_second = res / 60;
@@ -13,6 +18,15 @@ function time_for_speech(res) {
     }
 }
 
+var btn_flag = false;
+function clear() {
+    if (btn_flag == false) {
+        $('#end_button').toggle();
+        btn_flag = true;
+    }
+}
+
+// 스크립트의 바이트 수를 비트 연산을 통해 세주는 함수
 function byte_cnt(chr) {
     str_byte_len = 0;
     str_byte_len = (function(s, b, i, c) {
@@ -22,10 +36,13 @@ function byte_cnt(chr) {
     return str_byte_len.toString() + "바이트";
 }
 
+// 스크립트의 글자 수를 세서 출력해주는 함수
 function char_count() {
     document.getElementById('byte_count_results').innerText = byte_cnt(document.getElementById('textbox').value);
     document.getElementById("char_count_results").innerText = document.getElementById('textbox').value.replace(/(\s*)/g, "").length + "자";
 }
+
+// 스크립트 입력 창을 유동적으로 만들어주는 함수
 function resize(obj) {
     obj.style.height = '1px';
     obj.style.height = (30 + obj.scrollHeight) + 'px';
--- voice.js
+++ voice.js
@@ -1,10 +1,16 @@
-window.SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
+/*
+web sppech API를 이용한 음성 인식을 주로 구현해놓은 스크립트
+*/
 
+// 객체 생성
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+
 // 사용할 임시 변수들
 let st = 0; let en = 0;
 var final_res = 0;
+// 말하기 속도의 기준점을 저장해놓는 변수
+const Low = 320; const High = 420;
 // 마이크의 사용 유무
 var flag = false;
 
@@ -21,12 +27,13 @@ recognition.continuous = false;
 // maxAlternatives가 크면 이상한 단어도 문장에 적합하게 알아서 수정함
 recognition.maxAlternatives = 100000;
 
+// 적당한 위치에 인식한 텍스트를 저장하기 위해 새 객체를 만듬
 let p = document.createElement("p");
 p.classList.add("para");
-
 let words = document.querySelector(".words");
 words.appendChild(p);
 
+// 음성인식을 처리해서 문자열로 저장하는 함수
 let speechToText = "";
 let interimTranscript = "";
 recognition.addEventListener("result", (e) => {
@@ -45,13 +52,16 @@ recognition.addEventListener("result", (e) => {
     document.querySelector(".para").innerHTML = speechToText + interimTranscript;
 });
 
+// 분당 몇 글자를 읽는 지 계산한 후 예상시간이 얼마나 걸리는지 함수를 통해 가져와서 출력하는 함수
 function cal(res) {
     final_res = parseInt(60000 / (res / str_cnt));
     console.log(final_res);
     document.getElementById('time_results').innerText = time_for_speech(final_res);
 }
 
+// 마이크를 사용하지 못하는 환경일때 작동하는 함수
 recognition.addEventListener("error", () => {
+    $("#loader").toggle();
     document.getElementById("result_card").style.visibility = "hidden";
     st = new Date();
     console.log("인식 시작");
@@ -59,20 +69,22 @@ recognition.addEventListener("error", () => {
     $("#end_button").toggle();
 });
 
+// 마이크 인식이 끝날때 동시에 작동되는 핢수
 recognition.addEventListener("end", () => {
-    $("#loader").toggle();
 
-    if (flag == true) document.getElementById("result_card").style.visibility = "visible";
+    if (flag == true) {
+        document.getElementById("result_card").style.visibility = "visible";
+    }
 
     if (document.querySelector(".para").innerHTML == "") {
         //$("#loader").toggle();
         //document.getElementById("loader").innerText = "마이크를 인식하지 못했어요. 새로고침 후 다시 시도해주세요.";
         console.log("마이크 입력 안됨");
-        if (final_res > 400) {
+        if (final_res > High) {
             document.getElementById("card_2_main").innerText = "말하기 속도가 빠른 편이에요.";
             document.getElementById("card_2_sub").innerText = "1분에 약 " + final_res.toString() + "자 정도 말할수 있는 속도에요.";
         }
-        else if (final_res < 300) {
+        else if (final_res < Low) {
             document.getElementById("card_2_main").innerText = "말하기 속도가 느린 편이에요.";
             document.getElementById("card_2_sub").innerText = "1분에 약 " + final_res.toString() + "자 정도 말할수 있는 속도에요.";
         }
@@ -103,11 +115,11 @@ recognition.addEventListener("end", () => {
 
         }
 
-        if (final_res > 400) {
+        if (final_res > High) {
             document.getElementById("card_2_main").innerText = "말하기 속도가 빠른 편이에요.";
             document.getElementById("card_2_sub").innerText = "1분에 약 " + final_res.toString() + "자 정도 말할수 있는 속도에요.";
         }
-        else if (final_res < 300) {
+        else if (final_res < Low) {
             document.getElementById("card_2_main").innerText = "말하기 속도가 느린 편이에요.";
             document.getElementById("card_2_sub").innerText = "1분에 약 " + final_res.toString() + "자 정도 말할수 있는 속도에요.";
         }
@@ -118,6 +130,7 @@ recognition.addEventListener("end", () => {
     }   
 });
 
+// 인식이 시작할때 작동하는 함수
 recognition.addEventListener("start", () =>  {
     flag = true;
     st = new Date();
@@ -126,6 +139,8 @@ recognition.addEventListener("start", () =>  {
     $("#end_button").toggle();
     $("#loader").toggle();
 });
+
+// 종료 버튼을 눌렀을때 작동하는 함수
 function endSpeech() {
     en = new Date();
     cal(en - st); 
@@ -138,11 +153,11 @@ function endSpeech() {
     if (flag == false) {
         document.getElementById("result_card").style.visibility = "visible";
 
-        if (final_res > 400) {
+        if (final_res > High) {
             document.getElementById("card_2_main").innerText = "말하기 속도가 빠른 편이에요.";
             document.getElementById("card_2_sub").innerText = "1분에 약 " + final_res.toString() + "자 정도 말할수 있는 속도에요.";
         }
-        else if (final_res < 300) {
+        else if (final_res < Low) {
             document.getElementById("card_2_main").innerText = "말하기 속도가 느린 편이에요.";
             document.getElementById("card_2_sub").innerText = "1분에 약 " + final_res.toString() + "자 정도 말할수 있는 속도에요.";
         }
@@ -153,7 +168,11 @@ function endSpeech() {
 
         document.getElementById("card_3_main").innerText = "마이크 사용을 거부하거나 마이크가 안되는 거 같아요.";
         document.getElementById("card_3_sub").innerText = "새로고침 후 다시 시도하거나 마이크 사용을 승인해주세요.";
+
+        $("#loader").toggle();
     }
+
+    
 };
 
 // 음성 인식 시작
